0c3d40bf99a5f25927b6a574eedfd137cb1e7db6
kim4ky <kimaky121209@gmail.com>
2024-02-26 18:05:46 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, format: { with: /\A[a-z\dぁ-んァ-ヶ一-龥々ー]+\z/i }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :date_of_birth, presence: true
 end
