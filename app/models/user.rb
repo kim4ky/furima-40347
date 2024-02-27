2e1ef15f6a43476a3b380103c105bf63654d8d39
kim4ky <kimaky121209@gmail.com>
2024-02-27 22:09:54 +0900

    新規登録機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with: /\A(?=.*?[\d])(?=.*?[a-z])[a-z\d]+\z/i }
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
