9388fd7539dd10ca21ee8c9f67f3d63376391845
kim4ky <kimaky121209@gmail.com>
2024-03-01 14:40:30 +0900

    販売手数料、販売利益の計算結果表示の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const salesProfit = document.getElementById("profit");
+    const tax = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = tax;
+    const profit = Math.floor(inputValue - tax);
+    salesProfit.innerHTML = profit;
+  })
+});
